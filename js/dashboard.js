02594b68e89585aa4e1a76d4d08572007d7732a6
Butiza <butinyana2017@gmail.com>
2026-06-15 23:40:46 +0200

    Root 06: Style the landing page add a bit of functionality

--- js/dashboard.js
+++ js/dashboard.js
@@ -3,6 +3,10 @@ const params = new URLSearchParams(window.location.search);
 const vehicleId = params.get("id");
 
 const vehicle = vehicles[vehicleId];
+document.getElementById("vehicle-id").textContent =
+    "NVIP-2026-00" + vehicleId;
+
+const alertMessage = document.getElementById("alert-message");
 
 document.getElementById("status-text").textContent = vehicle.status;
 document.getElementById("registration").textContent = vehicle.registration;
@@ -25,4 +29,25 @@ else if (vehicle.status.includes("INVESTIGATE")) {
 }
 else if (vehicle.status.includes("HIGH PRIORITY")) {
     statusSection.classList.add("status-priority");
+}
+
+if (vehicle.status.includes("CLEAR")) {
+
+    alertMessage.textContent =
+        "No enforcement action required.";
+
+}
+
+else if (vehicle.status.includes("INVESTIGATE")) {
+
+    alertMessage.textContent =
+        "Outstanding compliance issues detected. Further verification recommended.";
+
+}
+
+else if (vehicle.status.includes("HIGH PRIORITY")) {
+
+    alertMessage.textContent =
+        "Reported stolen vehicle. Immediate investigation required.";
+
 }
